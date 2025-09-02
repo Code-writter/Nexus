8c860c05805d48d6ab63444232955ac53e58a199
abhishektiwari283 <142007480+Code-writter@users.noreply.github.com>
2025-09-02 23:19:51 +0530

    User details route

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,14 +1,17 @@
 import { Router } from "express";
 import {
+    handleUserInformation,
     handleRegisterUser,
     handleLoginUser
 } from '../controllers/user.controller.js'
+
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
 router.route('/register').post(handleRegisterUser)
 router.route('/login').post(handleLoginUser)
-    
+router.route("/information").get(verifyJWT, handleUserInformation)
 
 
 export default router
