16239909f8b08bf1c419d201614eebd3e8731a98
abhishektiwari283 <142007480+Code-writter@users.noreply.github.com>
2025-09-02 18:29:59 +0530

    method for the access token and refresh token

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,9 +1,29 @@
 import { asyncHandler } from '../utils/AsyncHandler.js'
+import { User } from '../models/user.model.js'
+
+const generateAccessAndRefreshToken = async (userId) => {
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({ validateBeforeSave: false })
+
+        return {accessToken, refreshToken}
+
+    } catch (error) {
+        throw new ApiError(500, `Something went wrong while generating refresh and access token ${error}`)
+    }
+}
+
 
 const handleUserInformation = asyncHandler( async (req, res) => {
-    return res.json({
-        msg : "Server working"
-    })
+    try {
+        
+    } catch (error) {
+        
+    }
 })
 
 
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -55,7 +55,6 @@ userSchema.methods.generateAccessToken = function(){
         {
             _id: this._id,
             email: this.email,
-            username: this.username,
             fullName: this.fullName
         },
         
