070d891e4d5d40ae4c65110bb7e1e48204562bde
abhishektiwari283 <142007480+Code-writter@users.noreply.github.com>
2025-09-02 17:03:18 +0530

    backend setup

---
+++ backend/database/connectDB.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import {config} from 'dotenv'
+
+config()
+
+export default async function connectDB(){
+    try {
+        const databaseUrl = process.env.MONGODB_URI
+        const appName = process.env.APP_NAME
+        const connectionInstance = await mongoose.connect(`${databaseUrl}${appName}`)
+
+        console.log(`Database connected ${connectionInstance.connection.host}`)
+    } catch (error) {
+        console.log(error)
+    }
+}
