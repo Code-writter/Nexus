66f1244a7f5eaf5df9cf885c06e9357a33e7785f
abhishektiwari283 <142007480+Code-writter@users.noreply.github.com>
2025-09-05 11:41:35 +0530

    dotenv path setup

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,15 @@ import app from './app.js'
 import connectDB from './database/connectDB.js'
 import { ApiError } from './utils/ApiError.js'
 
-// .env config
-dotenv.config({
-    path : "./env"
-})
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, './env') });
+
 
 const PORT = process.env.PORT || 8000
 
