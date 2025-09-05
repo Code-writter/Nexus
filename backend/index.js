271d6714ab99eaebae4f31b484da5cf9d9e4964b
abhishektiwari283 <142007480+Code-writter@users.noreply.github.com>
2025-09-05 13:02:21 +0530

    sever deployement errror

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,11 @@
 
 import dotenv from 'dotenv'
-
+dotenv.config()
 
 import app from './app.js'
 import connectDB from './database/connectDB.js'
 import { ApiError } from './utils/ApiError.js'
 
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-dotenv.config({ path: path.resolve(__dirname, './env') });
 
 
 const PORT = process.env.PORT || 8000
